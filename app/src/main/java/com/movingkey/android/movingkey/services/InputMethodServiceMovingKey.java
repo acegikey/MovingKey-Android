ac5a1e57438b508e03fbe79b0716b8dc47881dbe
whiteday910 <hwiPW7356!!>
2016-09-30 17:09:13 +0900

    소스 구조 주석 추가

--- app/src/main/java/com/movingkey/android/movingkey/services/InputMethodServiceMovingKey.java
+++ app/src/main/java/com/movingkey/android/movingkey/services/InputMethodServiceMovingKey.java
@@ -10,6 +10,21 @@ import com.movingkey.android.movingkey.R;
 import com.movingkey.android.movingkey.customLib.MVInputManager;
 import com.movingkey.android.movingkey.keyboardLayout.KeyboardContainerLayout;
 
+
+
+/*
+안드로이드 소스 구조 인수인계 자료
+
+        1. MovingKeySetting --> JSON 구조 파일로 저장되는 사용자 설정 값.
+        2. LangAndLayout --> 저장되는 설정 값
+        3. Const --> 상수값 모음
+        4. MovingKeyLib --> 키보드 공통 사용 라이브러리
+        5. InputMethodServiceMovingKey --> 키보드를 만들 때 생기는 최초 진입점. KeyboardContainerLayout 를 불러와서 표현한다.
+        6. KeyboardContainerLayout --> 설정에 맞는 레이아웃을 찾아서 뷰에 셋팅한다.
+
+*/
+
+
 public class InputMethodServiceMovingKey extends InputMethodService
 {
 
